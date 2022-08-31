1fab1a46c986bd4c00a73ce5fc74ec46194d3d2f
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 09:13:17 +0200

    Qiankun bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {renderWithQiankun, qiankunWindow} from 'vite-plugin-qiankun/dist/helper'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const retrieveContainer = props => props.container ?? document
+
+const render = (props) => {
+  const container = retrieveContainer(props)
+  ReactDOM.createRoot(container.querySelector('#root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+renderWithQiankun({
+  mount(props) {
+    render(props)
+  },
+  bootstrap() {},
+  unmount(props) {
+    ReactDOM.unmountComponentAtNode(retrieveContainer(props))
+  },
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
