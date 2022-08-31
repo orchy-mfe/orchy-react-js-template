bd6f2f6789e033a166347e6d327926fab7c7bb51
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 09:23:16 +0200

    add: license

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -5,7 +5,7 @@ import App from './App'
 
 describe('App', () => {
     it('renders correctly', () => {
-        render(<App></App>)
+        render(<App/>)
         expect(screen.getByText('Click on the Vite and React logos to learn more')).toBeDefined()
     })
 })
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import svgr from 'vite-plugin-svgr'
-import { visualizer } from 'rollup-plugin-visualizer'
+import {visualizer} from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 import qiankun from 'vite-plugin-qiankun'
 
 const port = process.env.PORT || 3000
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({mode}) => ({
   plugins: [
     svgr(),
     visualizer(),
-    react({ fastRefresh: false }),
-    qiankun('orchy-react-js-template', { useDevMode: true }),
+    react({fastRefresh: false}),
+    qiankun('orchy-react-js-template', {useDevMode: true}),
   ],
   base: mode === 'development' ? `http://localhost:${port}/` : '/orchy-react-js-template/',
-  server: { port },
+  server: {port},
   test: {
     environment: 'happy-dom',
     mockReset: true
