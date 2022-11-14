28b2698b92dc862fd3fe0171ce30c8d10028cc13
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-14 22:40:32 +0100

    feat: upgrade to version 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,26 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {renderWithQiankun, qiankunWindow} from 'vite-plugin-qiankun/dist/helper'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-let root
+import OrchyBaseMfe from '@orchy-mfe/spa-adapter'
+export class ReactMfe extends OrchyBaseMfe {
+  root
+  async mount(microFrontendProperties) {
+    this.root = createRoot(this.getContainer())
+    this.root.render(
+      <StrictMode>
+        <BrowserRouter basename={microFrontendProperties?.basePath}>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    )
+  }
 
-const retrieveContainer = props => props.container ?? document
-
-const render = (props = {}) => {
-  const container = retrieveContainer(props)
-  root = createRoot(container.querySelector('#root'))
-  root.render(
-    <StrictMode>
-      <BrowserRouter basename={props.baseUrl}>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  )
+  async unmount() {
+    this.root?.unmount()
+  }
 }
 
-renderWithQiankun({
-  mount(props) {
-    render(props)
-  },
-  bootstrap() {},
-  unmount() {
-    root.unmount()
-  },
-})
-
-if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-  render({})
-}
+customElements.define('react-mfe', ReactMfe)
